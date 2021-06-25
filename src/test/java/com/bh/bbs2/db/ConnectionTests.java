5eb04051ed7758d6306debdac57baaba4775d294
BrianH <brianhwang29@gmail.com>
2021-06-25 10:09:59 +0900

    mysql connection test

---
+++ src/test/java/com/bh/bbs2/db/ConnectionTests.java
@@ -0,0 +1,29 @@
+package com.bh.bbs2.db;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.Connection;
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ConnectionTests {
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void Connection_test(){
+        try(Connection con = sqlSessionFactory.openSession().getConnection()){
+            System.out.println("Connection success");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
